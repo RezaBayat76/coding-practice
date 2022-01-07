44747108ab78f090463064228ee3773e22c47ac5
A M <nil.objc@gmail.com>
2022-01-06 20:24:38 -0500

    add Amin folder

---
+++ Amin/704_Binary_Search.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        first = 0
+        last = len(nums) - 1
+        while first <= last:
+            mid = (first + last) // 2
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                first = mid + 1
+            elif target < nums(n/2):
+                last = mid -1
+        return -1
